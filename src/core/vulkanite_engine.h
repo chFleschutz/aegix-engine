147b819f2f7659bd8bae98ef2df7e0ab722c03bf
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-10-18 20:12:17 +0200

    Added frame brake

--- src/core/vulkanite_engine.h
+++ src/core/vulkanite_engine.h
@@ -18,6 +18,8 @@ namespace vre
 		static constexpr int WIDTH = 1080;
 		static constexpr int HEIGHT = 720;
 
+		static constexpr int MAX_FPS = 144; // Max frames per second, set 0 to disable
+
 		VulkaniteEngine();
 		~VulkaniteEngine();
 
@@ -37,6 +39,8 @@ namespace vre
 		}
 
 	private:
+		void applyFrameBrake(std::chrono::steady_clock::time_point frameBeginTime);
+
 		Window m_window{ WIDTH, HEIGHT, "Vulkanite" };
 		VulkanDevice m_device{ m_window };
 		Renderer m_renderer{ m_window, m_device };
