faee1d6f144e82b77a006a9868692a67997c22a9
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-10-21 00:12:38 +0200

    Added ability to add components and script from custom scripts

--- src/scene/script_component_base.h
+++ src/scene/script_component_base.h
@@ -8,17 +8,44 @@ namespace vre
 	class ScriptComponentBase
 	{
 	public:
+
 		virtual ~ScriptComponentBase() {}
 
 	protected:
+		/// @brief Called once at the beginning of the scene
 		virtual void begin() {}
+		/// @brief Called every frame with the delta of the last two frames in seconds
 		virtual void update(float deltaSeconds) {}
+		/// @brief Called once at the end of the scene
 		virtual void end() {}
 
+		/// @brief Returns true if the entity has all components of type T...
+		template<typename... T>
+		bool hasComponent()
+		{
+			return m_entity->hasComponent<T...>();
+		}
+
+		/// @brief Returns a reference of the component of type T
+		/// @note Component of type T must exist
 		template<typename T>
 		T& getComponent()
 		{
 			return m_entity.getComponent<T>();
+		}
+
+		/// @brief Adds a component of type T to the entity and returns a reference to it
+		template<typename T, typename... Args>
+		T& addComponent(Args&&... args)
+		{
+			return m_entity.addComponent<T>(std::forward<Args>(args)...);
+		}
+
+		/// @brief Adds a script of type T to the entity and returns a reference to it
+		template<typename T, typename... Args>
+		ScriptComponent& addScript(Args&&... args)
+		{
+			return m_entity.addScript<T>(std::forward<Args>(args)...);
 		}
 
 	private:
