b85dfb5562dd003a7f14a42d426517916fd73a3b
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-11-09 17:28:42 +0100

    Added convenience getter

--- src/physics/motion_dynamics.h
+++ src/physics/motion_dynamics.h
@@ -11,17 +11,24 @@ namespace VEPhysics
 		void addAcceleration(const Vector3& acceleration);
 		void addAngularAcceleration(const Vector3& angularAcceleration);
 
-	protected:
+		Vector3 velocity() const { return m_velocity; }
+		Vector3 angularVelocity() const { return m_angularVelocity; }
+
+		float speed() const { return glm::length(m_velocity); }
+		float angularSpeed() const { return glm::length(m_angularVelocity); }
+
+		Vector3 moveDirection() const { return glm::normalize(m_velocity); }
+
 		void update(float deltaSeconds) override;
 
 	private:
 		void applyAcceleration(float deltaSeconds);
 
-		Vector3 m_velocity;
-		Vector3 m_angularVelocity;
+		Vector3 m_velocity{ 0.0f };
+		Vector3 m_angularVelocity{ 0.0f };
 
-		Vector3 m_acceleration;
-		Vector3 m_angularAcceleration;
+		Vector3 m_acceleration{ 0.0f };
+		Vector3 m_angularAcceleration{ 0.0f };
 	};
 
 } // namespace VEPhysics
