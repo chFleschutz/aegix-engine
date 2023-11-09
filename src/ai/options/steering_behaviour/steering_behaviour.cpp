88250b3c7725d9f16064ee0884957d9d449d615b
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-11-09 23:55:32 +0100

    Updated steering behaviour to use new dynamics forces

--- src/ai/options/steering_behaviour/steering_behaviour.cpp
+++ src/ai/options/steering_behaviour/steering_behaviour.cpp
@@ -2,15 +2,5 @@
 
 namespace VEAI
 {
-	void SteeringBehaviour::updateOption(float deltaSeconds)
-	{
-		auto acceleration = computeAcceleration();
-		auto angularAcceleration = computeAngularAcceleration();
-
-		// TODO: Update accelerations
-		// auto& motionDynamics = getComponent<VEPhysics::MotionDynamics>();
-		// motionDynamics.addAcceleration(acceleration);
-		// motionDynamics.addAngularAcceleration(angularAcceleration);
-	}
 
 } // namespace VEAI
--- src/ai/options/steering_behaviour/steering_behaviour.h
+++ src/ai/options/steering_behaviour/steering_behaviour.h
@@ -1,22 +1,24 @@
 #pragma once
 
+#include "ai/ai_component.h"
 #include "ai/options/option.h"
+#include "physics/motion_dynamics.h"
 #include "utils/math_utils.h"
 
 namespace VEAI
 {
 	class SteeringBehaviour : public Option
 	{
-	public:
-
 	protected:
-		virtual void updateOption(float deltaSeconds) override;
+		virtual void updateOption(float deltaSeconds) override
+		{
+			auto& dynamics = m_aiComponent->getComponent<VEPhysics::MotionDynamics>();
+			dynamics.addForce(computeForce());
+			dynamics.addAngularForce(computeAngularForce());
+		}
 
-		virtual Vector3 computeAcceleration() = 0;
-		virtual Vector3 computeAngularAcceleration() = 0;
-
-	private:
-
+		virtual Vector3 computeForce() = 0;
+		virtual Vector3 computeAngularForce() = 0;
 	};
 
 } // namespace VEAI
