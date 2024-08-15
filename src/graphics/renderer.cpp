4889c6ba9d8415574f614e49229039fa7c63e62c
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-08-15 22:37:40 +0200

    Fix using correct color format

--- src/graphics/renderer.cpp
+++ src/graphics/renderer.cpp
@@ -107,7 +107,7 @@ namespace Aegix::Graphics
 		renderPassInfo.renderArea.extent = { m_swapChain->swapChainExtent() };
 
 		std::array<VkClearValue, 2> clearValues{};
-		clearValues[0].color = { 0.01f, 0.01f, 0.01f, 1.0f };
+		clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f };
 		clearValues[1].depthStencil = { 1.0f, 0 };
 		renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
 		renderPassInfo.pClearValues = clearValues.data();
