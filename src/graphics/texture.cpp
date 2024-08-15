a189ca6042b9a0ac91cb900ebe40472602620e88
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-08-15 16:33:34 +0200

    Rework texture class

--- src/graphics/texture.cpp
+++ src/graphics/texture.cpp
@@ -9,12 +9,12 @@
 
 namespace Aegix::Graphics
 {
-	Texture::Texture(VulkanDevice& device, const Texture::CreateInfo& createInfo)
+	Texture::Texture(VulkanDevice& device, const std::filesystem::path& texturePath, const Texture::Config& config)
 		: m_device{ device }
 	{
-		loadTexture(createInfo.textureFilePath);
+		loadTexture(texturePath);
 		createImageView();
-		createTextureSampler();
+		createTextureSampler(config.addressMode, config.magFilter, config.minFilter);
 	}
 
 	Texture::~Texture()
@@ -34,24 +34,21 @@ namespace Aegix::Graphics
 		return info;
 	}
 
-	void Texture::loadTexture(const std::string& filePath)
+	void Texture::loadTexture(const std::filesystem::path& filePath)
 	{
 		int texWidth, texHeight, texChannels;
-		stbi_uc* pixels = stbi_load(filePath.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
-		VkDeviceSize imageSize = static_cast<uint64_t>(texWidth) * static_cast<uint64_t>(texHeight) * 4;
+		stbi_uc* pixels = stbi_load(filePath.string().c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+		VkDeviceSize imageSize = 4 * static_cast<VkDeviceSize>(texWidth) * static_cast<VkDeviceSize>(texHeight);
 
 		if (!pixels)
 			throw std::runtime_error("Failed to load texture image");
 
-		Buffer stagingBuffer{
-			m_device,
-			imageSize,
-			1,
-			VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+		Buffer stagingBuffer{m_device, imageSize, 1, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
 			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT };
 
 		stagingBuffer.map();
-		stagingBuffer.writeToBuffer((void*)pixels);
+		stagingBuffer.writeToBuffer(pixels);
+		stagingBuffer.unmap();
 
 		stbi_image_free(pixels);
 
@@ -66,14 +63,18 @@ namespace Aegix::Graphics
 		imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
 		imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
 		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-		imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+		imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
 		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.flags = 0;
 
-		m_device.createImageWithInfo(imageInfo, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_textureImage, m_textureImageMemory);
-		m_device.transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1);
+		m_device.createImage(imageInfo, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_textureImage, m_textureImageMemory);
+
+		m_device.transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, 
+			VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 		m_device.copyBufferToImage(stagingBuffer.buffer(), m_textureImage, imageInfo.extent.width, imageInfo.extent.height, 1);
-		m_device.transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 1);
+		m_device.transitionImageLayout(m_textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 
+			VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 	}
 
 	void Texture::createImageView()
@@ -93,17 +94,17 @@ namespace Aegix::Graphics
 			throw std::runtime_error("failed to create texture image view");
 	}
 
-	void Texture::createTextureSampler()
+	void Texture::createTextureSampler(VkSamplerAddressMode addressMode, VkFilter magFilter, VkFilter minFilter)
 	{
 		VkSamplerCreateInfo samplerInfo{};
 		samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-		samplerInfo.magFilter = VK_FILTER_LINEAR;
-		samplerInfo.minFilter = VK_FILTER_LINEAR;
-		samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-		samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-		samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+		samplerInfo.magFilter = magFilter;
+		samplerInfo.minFilter = minFilter;
+		samplerInfo.addressModeU = addressMode;
+		samplerInfo.addressModeV = addressMode;
+		samplerInfo.addressModeW = addressMode;
 		samplerInfo.anisotropyEnable = VK_TRUE;
-		samplerInfo.maxAnisotropy = m_device.properties.limits.maxSamplerAnisotropy;
+		samplerInfo.maxAnisotropy = m_device.properties().limits.maxSamplerAnisotropy;
 		samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
 		samplerInfo.unnormalizedCoordinates = VK_FALSE;
 		samplerInfo.compareEnable = VK_FALSE;
