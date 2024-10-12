237786d1881184bf54122839c97cc5b624cbe180
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-10-12 18:27:29 +0200

    Add function for checking depth format

--- src/graphics/vulkan_tools.h
+++ src/graphics/vulkan_tools.h
@@ -20,4 +20,7 @@ namespace Aegix::Tools
 {
 	/// @brief Returns the result as a string
 	std::string_view resultString(VkResult result);
+
+	/// @brief Returns true if the format is a depth or depth-stencil format otherwise false
+	bool isDepthFormat(VkFormat format);
 }
