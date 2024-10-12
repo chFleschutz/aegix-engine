a0460ed637da28ae5cd14594fb115e8e8b98d555
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-10-12 17:52:08 +0200

    Add attachments to frame graph render pass

--- src/graphics/frame_graph/frame_graph_render_pass.h
+++ src/graphics/frame_graph/frame_graph_render_pass.h
@@ -1,14 +1,25 @@
 #pragma once
 
 #include "graphics/frame_info.h"
+#include "graphics/texture.h"
 
 namespace Aegix::Graphics
 {
+	struct Attachment
+	{
+		std::shared_ptr<Texture> texture;
+	};
+
 	class FrameGraphRenderPass
 	{
 	public:
 		virtual ~FrameGraphRenderPass() = default;
 
 		virtual void execute(const FrameInfo& frameInfo) = 0;
+
+		void setAttachments(std::vector<Attachment> attachments) { m_attachments = std::move(attachments); }
+
+	private:
+		std::vector<Attachment> m_attachments;
 	};
 }
