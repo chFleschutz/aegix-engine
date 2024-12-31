709fad87b80a9cae2996be51e8dead7095e9a905
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-31 16:37:33 +0100

    Refactor FrameGraphNode

--- src/graphics/frame_graph/frame_graph.h
+++ src/graphics/frame_graph/frame_graph.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #include "graphics/frame_graph/frame_graph_node.h"
+#include "graphics/frame_graph/frame_graph_node.h"
 #include "graphics/frame_graph/frame_graph_pass.h"
 #include "graphics/frame_graph/frame_graph_resource.h"
 
-#include <algorithm>
-#include <cassert>
-#include <functional>
 #include <memory>
 #include <string>
 #include <vector>
@@ -24,43 +22,9 @@ namespace Aegix::Graphics
 			uint32_t width;
 			uint32_t height;
 		};
-
 	};
+	
 
-	class PassNode : public FrameGraphNode
-	{
-	public:
-		friend class FrameGraph;
-
-		PassNode(std::string_view name, uint32_t id, std::unique_ptr<FrameGraphPassBase> pass)
-			: FrameGraphNode{ name, id }, m_pass{ std::move(pass) }
-		{
-		}
-
-		void execute() const
-		{
-			std::invoke(*m_pass);
-		}
-
-		void addRead(FrameGraphResourceID resource)
-		{
-			assert(std::find(m_reads.begin(), m_reads.end(), resource) == m_reads.end() && 
-				"Resource already declared as read");
-			m_reads.emplace_back(resource);
-		}
-
-		void addWrite(FrameGraphResourceID resource)
-		{
-			assert(std::find(m_writes.begin(), m_writes.end(), resource) == m_writes.end() &&
-				"Resource already declared as write");
-			m_writes.emplace_back(resource);
-		}
-
-	private:
-		std::unique_ptr<FrameGraphPassBase> m_pass;
-		std::vector<FrameGraphResourceID> m_reads;
-		std::vector<FrameGraphResourceID> m_writes;
-	};
 
 	class FrameGraph
 	{
@@ -70,32 +34,25 @@ namespace Aegix::Graphics
 		class Builder
 		{
 		public:
-			Builder(FrameGraph& frameGraph, PassNode& node)
+			Builder(FrameGraph& frameGraph, FrameGraphNode& node)
 				: m_frameGraph{ frameGraph }, m_node{ node }
 			{
 			}
 
 			template <typename T>
 				requires HasDesc<T>
-			[[nodiscard]] 
+			[[nodiscard]]
 			auto create(const std::string_view name, const typename T::Desc& desc) -> FrameGraphResourceID
 			{
-				return m_frameGraph.createResource<T>(name, desc);
+				return m_frameGraph.addResource<T>(name, desc);
 			}
 
-			void declareRead(FrameGraphResourceID resource) 
-			{
-				m_node.addRead(resource);
-			}
-
-			void declareWrite(FrameGraphResourceID resource)
-			{
-				m_node.addWrite(resource);
-			}
+			void declareRead(FrameGraphResourceID resource) { m_node.addRead(resource); }
+			void declareWrite(FrameGraphResourceID resource) { m_node.addWrite(resource); }
 
 		private:
 			FrameGraph& m_frameGraph;
-			PassNode& m_node;
+			FrameGraphNode& m_node;
 		};
 
 		template <typename Data = NoData, typename Setup, typename Execute>
@@ -108,12 +65,20 @@ namespace Aegix::Graphics
 			auto pass = std::make_unique<FrameGraphPass<Data, Execute>>(std::forward<Execute>(execute));
 			auto& data = pass->data;
 
-			const auto id = static_cast<uint32_t>(m_passes.size());
-			auto& node = m_passes.emplace_back(name, id, std::move(pass));
+			const auto id = static_cast<FrameGraphNodeID>(m_nodes.size());
+			auto& node = m_nodes.emplace_back(name, id, std::move(pass));
 
 			Builder builder{ *this, node };
 			std::invoke(setup, builder, data);
 			return data;
+		}
+
+		template <typename T>
+		auto addResource(const std::string_view name, const typename T::Desc& desc) -> FrameGraphResourceID
+		{
+			const auto id = static_cast<FrameGraphResourceID>(m_resources.size());
+			m_resources.emplace_back(id, desc, T{});
+			return id;
 		}
 
 		void compile()
@@ -123,22 +88,14 @@ namespace Aegix::Graphics
 
 		void execute()
 		{
-			for (auto& pass : m_passes)
+			for (auto& node : m_nodes)
 			{
-				pass.execute();
+				node.executePass();
 			}
 		}
 
 	private:
-		template <typename T>
-		auto createResource(const std::string_view name, const typename T::Desc& desc) -> FrameGraphResourceID
-		{
-			const auto id = static_cast<FrameGraphResourceID>(m_resources.size());
-			m_resources.emplace_back(id, desc, T{});
-			return id;
-		}
-
-		std::vector<PassNode> m_passes;
+		std::vector<FrameGraphNode> m_nodes;
 		std::vector<FrameGraphResource> m_resources;
 	};
 }
--- src/graphics/frame_graph/frame_graph_node.h
+++ src/graphics/frame_graph/frame_graph_node.h
@@ -1,19 +1,52 @@
 #pragma once
 
+#include "graphics/frame_graph/frame_graph_pass.h"
+#include "graphics/frame_graph/frame_graph_resource.h"
+
+#include <algorithm>
+#include <cassert>
+#include <memory>
 #include <string>
+#include <vector>
 
 namespace Aegix::Graphics
 {
+	using FrameGraphNodeID = uint32_t;
+
 	class FrameGraphNode
 	{
 	public:
-		FrameGraphNode(std::string_view name, uint32_t id)
-			: m_name{ name }, m_id{ id }
+		friend class FrameGraph;
+
+		FrameGraphNode(std::string_view name, FrameGraphNodeID id, std::unique_ptr<FrameGraphPassBase> pass)
+			: m_name{ name }, m_id{ id }, m_pass{ std::move(pass) }
 		{
+		}
+
+		void executePass()
+		{
+			std::invoke(*m_pass);
+		}
+
+		void addRead(FrameGraphResourceID resource)
+		{
+			assert(std::find(m_reads.begin(), m_reads.end(), resource) == m_reads.end() &&
+				"Resource already declared as read");
+			m_reads.emplace_back(resource);
+		}
+
+		void addWrite(FrameGraphResourceID resource)
+		{
+			assert(std::find(m_writes.begin(), m_writes.end(), resource) == m_writes.end() &&
+				"Resource already declared as write");
+			m_writes.emplace_back(resource);
 		}
 
 	private:
 		std::string m_name;
-		uint32_t m_id;
+		FrameGraphNodeID m_id;
+		std::unique_ptr<FrameGraphPassBase> m_pass;
+		std::vector<FrameGraphResourceID> m_reads;
+		std::vector<FrameGraphResourceID> m_writes;
 	};
 }
