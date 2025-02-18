ab135c6264b0ad8973c6652b4b9d0e3d40ed5e0a
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-02-18 19:29:16 +0100

    Add templated setData functions

--- src/graphics/uniform_buffer.h
+++ src/graphics/uniform_buffer.h
@@ -12,6 +12,19 @@ namespace Aegix::Graphics
 	class UniformBuffer
 	{
 	public:
+		template<typename T>
+		UniformBuffer(VulkanDevice& device, const T& data)
+		{
+			for (auto& buffer : m_buffers)
+			{
+				buffer = std::make_unique<Buffer>(device, sizeof(T), 1, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+					VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+				buffer->map();
+				buffer->writeToBuffer(&data, sizeof(T));
+				buffer->flush();
+			}
+		}
+
 		UniformBuffer(VulkanDevice& device, const void* data, size_t size)
 		{
 			for (auto& buffer : m_buffers)
@@ -30,11 +43,12 @@ namespace Aegix::Graphics
 		/// @brief Sets the data in the buffer for all frames
 		/// @param data The new data
 		/// @note This function should not be called once rendering has started (call setData(data, index) instead)
-		void setData(const void* data)
+		template<typename T>
+		void setData(const T& data, VkDeviceSize offset = 0)
 		{
 			for (auto& buffer : m_buffers)
 			{
-				buffer->writeToBuffer(data);
+				buffer->writeToBuffer(&data, sizeof(T), offset);
 			}
 		}
 
@@ -42,9 +56,10 @@ namespace Aegix::Graphics
 		/// @param data The new data
 		/// @param index The index of the currently active frame
 		/// @note This function can be savely called during rendering
-		void setData(int index, const void* data)
+		template<typename T>
+		void setData(int index, const T& data, VkDeviceSize offset = 0)
 		{
-			m_buffers[index]->writeToBuffer(data);
+			m_buffers[index]->writeToBuffer(&data, sizeof(T), offset);
 			m_buffers[index]->flush();
 		}
 
@@ -72,13 +87,13 @@ namespace Aegix::Graphics
 		void setData(const T& data)
 		{
 			m_data = data;
-			m_buffer.setData(&data);
+			m_buffer.setData(data);
 		}
 
 		void setData(int index, const T& data)
 		{
 			m_data = data;
-			m_buffer.setData(index, &data);
+			m_buffer.setData(index, data);
 		}
 
 	private:
