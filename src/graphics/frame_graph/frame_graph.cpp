2dff80f3550cc50df67696493729b24efc77ae2b
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-02-19 16:36:56 +0100

    Refactor frame graph

--- src/graphics/frame_graph/frame_graph.cpp
+++ src/graphics/frame_graph/frame_graph.cpp
@@ -2,5 +2,105 @@
 
 namespace Aegix::Graphics
 {
+	// FrameGraph::Builder -------------------------------------------------------
 
+	FrameGraph::Builder::Builder(FrameGraph& frameGraph, FrameGraphNode& node)
+		: m_frameGraph{ frameGraph }, m_node{ node }
+	{
+	}
+
+	FrameGraphResourceID FrameGraph::Builder::declareRead(FrameGraphResourceID resource)
+	{
+		m_node.addRead(resource);
+		return resource;
+	}
+
+	FrameGraphResourceID FrameGraph::Builder::declareWrite(FrameGraphResourceID resource)
+	{
+		m_node.addWrite(resource);
+		return resource;
+	}
+
+
+
+	// FrameGraph ----------------------------------------------------------------
+
+	FrameGraphResourceID FrameGraph::addTexture(VulkanDevice& device, const std::string& name, const FrameGraphTexture::Desc& desc)
+	{
+		return m_resourcePool.addTexture(device, name, desc);
+	}
+
+	void FrameGraph::compile()
+	{
+		// TODO
+	}
+
+	void FrameGraph::execute(const FrameInfo& frameInfo)
+	{
+		for (auto& node : m_nodes)
+		{
+			placeBarriers(frameInfo.commandBuffer, node);
+
+			node.executePass(m_resourcePool, frameInfo);
+		}
+	}
+
+	void FrameGraph::swapChainResized()
+	{
+		// TODO
+	}
+
+	void FrameGraph::placeBarriers(VkCommandBuffer commandBuffer, FrameGraphNode& node)
+	{
+		BarrierPlacement readColor{
+			.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+			.srcStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+			.dstStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT
+		};
+		BarrierPlacement readDepth{
+			.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+			.srcStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+			.dstStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT
+		};
+		placeBarriers(commandBuffer, node.reads(), readColor, readDepth);
+
+		BarrierPlacement writeColor{
+			.newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+			.srcStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+			.dstStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+		};
+		BarrierPlacement writeDepth{
+			.newLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+			.srcStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+			.dstStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT
+		};
+		placeBarriers(commandBuffer, node.writes(), writeColor, writeDepth);
+	}
+
+	void FrameGraph::placeBarriers(VkCommandBuffer commandBuffer, const std::vector<FrameGraphResourceID>& resources,
+		const BarrierPlacement& color, const BarrierPlacement& depth)
+	{
+		std::vector<VkImageMemoryBarrier> depthBarriers;
+		std::vector<VkImageMemoryBarrier> colorBarriers;
+		colorBarriers.reserve(resources.size());
+
+		for (auto& resource : resources)
+		{
+			Texture& texture = m_resourcePool.texture(resource).texture;
+
+			if (Tools::isDepthFormat(texture.format()))
+			{
+				if (texture.layout() != depth.newLayout)
+					depthBarriers.emplace_back(texture.imageMemoryBarrier(depth.newLayout));
+			}
+			else
+			{
+				if (texture.layout() != color.newLayout)
+					colorBarriers.emplace_back(texture.imageMemoryBarrier(color.newLayout));
+			}
+		}
+
+		Tools::vk::cmdPipelineBarrier(commandBuffer, depth.srcStage, depth.dstStage, depthBarriers);
+		Tools::vk::cmdPipelineBarrier(commandBuffer, color.srcStage, color.dstStage, colorBarriers);
+	}
 }
--- src/graphics/frame_graph/frame_graph.h
+++ src/graphics/frame_graph/frame_graph.h
@@ -28,45 +28,14 @@ namespace Aegix::Graphics
 		class Builder
 		{
 		public:
-			Builder(FrameGraph& frameGraph, FrameGraphNode& node)
-				: m_frameGraph{ frameGraph }, m_node{ node }
-			{
-			}
-
-			FrameGraphResourceID declareRead(FrameGraphResourceID resource) 
-			{ 
-				m_node.addRead(resource); 
-				return resource;
-			}
-
-			FrameGraphResourceID declareWrite(FrameGraphResourceID resource) 
-			{ 
-				m_node.addWrite(resource);
-				return resource;
-			}
-
+			Builder(FrameGraph& frameGraph, FrameGraphNode& node);
+			FrameGraphResourceID declareRead(FrameGraphResourceID resource);
+			FrameGraphResourceID declareWrite(FrameGraphResourceID resource);
+			
 		private:
 			FrameGraph& m_frameGraph;
 			FrameGraphNode& m_node;
 		};
-
-		template <typename Data = NoData, typename Setup, typename Execute>
-			requires std::is_invocable_v<Setup, Builder&, Data&> && 
-					 std::is_invocable_v<Execute, const Data&, FrameGraphResourcePool&, const FrameInfo&> &&
-					 (sizeof(Execute) < 1024)
-		[[nodiscard]]
-		const Data& addPass(const std::string& name, Setup&& setup, Execute&& execute)
-		{
-			auto pass = std::make_unique<FrameGraphPass<Data, Execute>>(std::forward<Execute>(execute));
-			auto& data = pass->data;
-
-			const auto id = static_cast<FrameGraphNodeID>(m_nodes.size());
-			auto& node = m_nodes.emplace_back(name, id, std::move(pass));
-
-			Builder builder{ *this, node };
-			std::invoke(std::forward<Setup>(setup), builder, data);
-			return data;
-		}
 
 		FrameGraph() = default;
 		FrameGraph(const FrameGraph&) = delete;
@@ -76,106 +45,39 @@ namespace Aegix::Graphics
 		FrameGraph& operator=(const FrameGraph&) = delete;
 		FrameGraph& operator=(FrameGraph&&) = delete;
 
-		[[nodiscard]]
-		auto addTexture(VulkanDevice& device, const std::string& name, const FrameGraphTexture::Desc& desc) -> FrameGraphResourceID
+		template <typename Data = NoData, typename Setup, typename Execute>
+			requires std::is_invocable_v<Setup, Builder&, Data&> && 
+					 std::is_invocable_v<Execute, const Data&, FrameGraphResourcePool&, const FrameInfo&> &&
+					 (sizeof(Execute) < 1024)
+		[[nodiscard]] auto addPass(const std::string& name, Setup&& setup, Execute&& execute) -> const Data&
 		{
-			return m_resourcePool.addTexture(device, name, desc);
+			auto pass = std::make_unique<FrameGraphPass<Data, Execute>>(std::forward<Execute>(execute));
+			auto& data = pass->data;
+			auto& node = m_nodes.emplace_back(name, static_cast<FrameGraphNodeID>(m_nodes.size()), std::move(pass));
+			Builder builder{ *this, node };
+			std::invoke(std::forward<Setup>(setup), builder, data);
+			return data;
 		}
 
-		void compile()
-		{
-			// TODO
-		}
+		[[nodiscard]] auto addTexture(VulkanDevice& device, const std::string& name, const FrameGraphTexture::Desc& desc) -> FrameGraphResourceID;
 
-		void execute(const FrameInfo& frameInfo)
-		{
-			for (auto& node : m_nodes)
-			{
-				placeBarriers(frameInfo.commandBuffer, node);
+		[[nodiscard]] auto resourcePool() -> FrameGraphResourcePool& { return m_resourcePool; }
 
-				node.executePass(m_resourcePool, frameInfo);
-			}
-		}
-
-		void swapChainResized()
-		{
-			// TODO
-		}
-
-		auto resourcePool() -> FrameGraphResourcePool& { return m_resourcePool; }
+		void compile();
+		void execute(const FrameInfo& frameInfo);
+		void swapChainResized();
 
 	private:
-		void placeBarriers(VkCommandBuffer commandBuffer, FrameGraphNode& node)
+		struct BarrierPlacement
 		{
-			std::vector<VkImageMemoryBarrier> readDepthBarriers;
-			std::vector<VkImageMemoryBarrier> readColorBarriers;
-			readColorBarriers.reserve(node.reads().size());
+			VkImageLayout newLayout;
+			VkPipelineStageFlagBits srcStage;
+			VkPipelineStageFlagBits dstStage;
+		};
 
-			for (auto& read : node.reads())
-			{
-				Texture& texture = m_resourcePool.texture(read).texture;
-
-				VkImageLayout newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-				
-				if (texture.layout() == newLayout)
-					continue;
-
-				if (Tools::isDepthFormat(texture.format()))
-				{
-					readDepthBarriers.emplace_back(texture.imageMemoryBarrier(newLayout));
-				}
-				else
-				{
-					readColorBarriers.emplace_back(texture.imageMemoryBarrier(newLayout));
-				}
-			}
-
-			Tools::vk::cmdPipelineBarrier(commandBuffer,
-				VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-				VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-				readDepthBarriers
-			);
-
-			Tools::vk::cmdPipelineBarrier(commandBuffer,
-				VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-				VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-				readColorBarriers
-			);
-
-			std::vector<VkImageMemoryBarrier> writeDepthBarriers;
-			std::vector<VkImageMemoryBarrier> writeColorBarriers;
-			writeColorBarriers.reserve(node.writes().size());
-
-			for (auto& write : node.writes())
-			{
-				Texture& texture = m_resourcePool.texture(write).texture;
-
-				if (Tools::isDepthFormat(texture.format()))
-				{
-					VkImageLayout newLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-					if (texture.layout() != newLayout)
-						writeDepthBarriers.emplace_back(texture.imageMemoryBarrier(newLayout));
-				}
-				else
-				{
-					VkImageLayout newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-					if (texture.layout() != newLayout)
-						writeColorBarriers.emplace_back(texture.imageMemoryBarrier(newLayout));
-				}
-			}
-
-			Tools::vk::cmdPipelineBarrier(commandBuffer,
-				VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-				VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-				writeDepthBarriers
-			);
-
-			Tools::vk::cmdPipelineBarrier(commandBuffer,
-				VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-				VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-				writeColorBarriers
-			);
-		}
+		void placeBarriers(VkCommandBuffer commandBuffer, FrameGraphNode& node);
+		void placeBarriers(VkCommandBuffer commandBuffer, const std::vector<FrameGraphResourceID>& resources,
+			const BarrierPlacement& color, const BarrierPlacement& depth);
 
 		std::vector<FrameGraphNode> m_nodes;
 		FrameGraphResourcePool m_resourcePool;
