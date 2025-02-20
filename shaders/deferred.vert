9ad91a6cacc9c45daf273b54a2b33b3bc38d2aed
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-02-20 22:19:33 +0100

    Enabled backface culling

--- shaders/deferred.vert
+++ shaders/deferred.vert
@@ -6,8 +6,8 @@ void main()
 {
 	vec2 verts[3] = vec2[](
 		vec2(-1.0, -1.0),
-		vec2(3.0, -1.0),
-		vec2(-1.0, 3.0)
+		vec2(-1.0, 3.0),
+		vec2(3.0, -1.0)
 	);
 
 	gl_Position = vec4(verts[gl_VertexIndex], 0.0, 1.0);
--- src/graphics/pipeline.cpp
+++ src/graphics/pipeline.cpp
@@ -212,8 +212,8 @@ namespace Aegix::Graphics
 		configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
 		configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
 		configInfo.rasterizationInfo.lineWidth = 1.0f;
-		configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
-		configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+		configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+		configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 		configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
 		configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;
 		configInfo.rasterizationInfo.depthBiasClamp = 0.0f;
