c8c22e3036c95929b4f9735ddc33810e359cecee
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-01 13:41:07 +0100

    Add cpp file for lighting pass

--- src/graphics/render_passes/lighting_pass.h
+++ src/graphics/render_passes/lighting_pass.h
@@ -2,7 +2,6 @@
 
 #include "graphics/frame_graph/frame_graph_render_pass.h"
 #include "graphics/pipeline.h"
-#include "graphics/vulkan_tools.h"
 
 namespace Aegix::Graphics
 {
@@ -36,127 +35,15 @@ namespace Aegix::Graphics
 	class LightingPass : public FrameGraphRenderPass
 	{
 	public:
-		LightingPass(VulkanDevice& device, DescriptorPool& pool)
-		{
-			m_descriptorSetLayout = DescriptorSetLayout::Builder(device)
-				.addBinding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(1, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(2, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(3, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(4, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(5, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_COMPUTE_BIT)
-				.addBinding(6, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT)
-				.build();
+		LightingPass(VulkanDevice& device, DescriptorPool& pool);
 
-			m_descriptorSet = std::make_unique<DescriptorSet>(pool, *m_descriptorSetLayout);
-
-			m_ubo = std::make_unique<UniformBufferData<Lighting>>(device);
-
-			m_pipelineLayout = PipelineLayout::Builder(device)
-				.addDescriptorSetLayout(*m_descriptorSetLayout)
-				.build();
-
-			m_pipeline = Pipeline::ComputeBuilder(device, *m_pipelineLayout)
-				.setShaderStage(SHADER_DIR "lighting.comp.spv")
-				.build();
-		}
-
-		virtual auto createInfo(FrameGraphResourceBuilder& builder) -> FrameGraphNodeCreateInfo override
-		{
-			m_position = builder.add({ "Position", 
-				FrameGraphResourceType::Reference, 
-				FrameGraphResourceUsage::Compute
-				});
-			m_normal = builder.add({ "Normal",
-				FrameGraphResourceType::Reference,
-				FrameGraphResourceUsage::Compute
-				});
-			m_albedo = builder.add({ "Albedo",
-				FrameGraphResourceType::Reference,
-				FrameGraphResourceUsage::Compute
-				});
-			m_arm = builder.add({ "ARM",
-				FrameGraphResourceType::Reference,
-				FrameGraphResourceUsage::Compute
-				});
-			m_emissive = builder.add({ "Emissive",
-				FrameGraphResourceType::Reference,
-				FrameGraphResourceUsage::Compute
-				});
-			m_sceneColor = builder.add({ "SceneColor",
-				FrameGraphResourceType::Texture,
-				FrameGraphResourceUsage::Compute,
-				FrameGraphResourceTextureInfo{
-					.format = VK_FORMAT_R16G16B16A16_SFLOAT,
-					.extent = { 0, 0 },
-					.resizePolicy = ResizePolicy::SwapchainRelative
-					} 
-				});
-
-			return FrameGraphNodeCreateInfo{
-				.name = "Lighting",
-				.inputs = { m_position, m_normal, m_albedo, m_arm, m_emissive },
-				.outputs = { m_sceneColor }
-			};
-		}
-
-		virtual void execute(FrameGraphResourcePool& resources, const FrameInfo& frameInfo) override
-		{
-			VkCommandBuffer cmd = frameInfo.commandBuffer;
-
-			updateLightingUBO(frameInfo);
-
-			DescriptorWriter{ *m_descriptorSetLayout }
-				.writeImage(0, resources.texture(m_sceneColor))
-				.writeImage(1, resources.texture(m_position))
-				.writeImage(2, resources.texture(m_normal))
-				.writeImage(3, resources.texture(m_albedo))
-				.writeImage(4, resources.texture(m_arm))
-				.writeImage(5, resources.texture(m_emissive))
-				.writeBuffer(6, m_ubo->descriptorBufferInfo(frameInfo.frameIndex))
-				.build(m_descriptorSet->descriptorSet(frameInfo.frameIndex));
-
-			m_pipeline->bind(cmd);
-			m_descriptorSet->bind(cmd, *m_pipelineLayout, frameInfo.frameIndex, VK_PIPELINE_BIND_POINT_COMPUTE);
-
-			Tools::vk::cmdDispatch(cmd, frameInfo.swapChainExtent, { 16, 16 });
-		}
+		virtual auto createInfo(FrameGraphResourceBuilder& builder) -> FrameGraphNodeCreateInfo override;
+		virtual void execute(FrameGraphResourcePool& resources, const FrameInfo& frameInfo) override;
+		virtual void drawUI() override;
 
 	private:
-		void updateLightingUBO(const FrameInfo& frameInfo)
-		{
-			Lighting ubo{};
+		void updateLightingUBO(const FrameInfo& frameInfo);
 
-			ubo.cameraPosition = glm::vec4(frameInfo.scene.camera().getComponent<Component::Transform>().location, 1.0f);
-
-			ubo.ambient = {
-				.color = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f)
-			};
-
-			ubo.directional = {
-				.direction = glm::vec4(glm::normalize(glm::vec3(1.0f, 1.0f, 1.0f)), 0.0f),
-				.color = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f)
-			};
-
-			int32_t lighIndex = 0;
-			auto view = frameInfo.scene.viewEntities<Aegix::Component::Transform, Aegix::Component::PointLight>();
-			for (auto&& [entity, transform, pointLight] : view.each())
-			{
-				assert(lighIndex < MAX_POINT_LIGHTS && "Point lights exceed maximum number of point lights");
-				ubo.pointLights[lighIndex] = Lighting::PointLight{
-					.position = glm::vec4(transform.location, 1.0f),
-					.color = glm::vec4(pointLight.color, pointLight.intensity)
-				};
-				lighIndex++;
-
-				assert(lighIndex < 4);
-			}
-			ubo.pointLightCount = lighIndex;
-
-			m_ubo->setData(frameInfo.frameIndex, ubo);
-		}
-
-	private:
 		FrameGraphResourceHandle m_sceneColor;
 		FrameGraphResourceHandle m_position;
 		FrameGraphResourceHandle m_normal;
