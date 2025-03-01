acced4d3df92f6e3547eb5b576d74086929bac85
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-01 12:58:58 +0100

    Refactor resources table

--- src/ui/layers/renderer_layer.cpp
+++ src/ui/layers/renderer_layer.cpp
@@ -24,6 +24,34 @@ namespace Aegix::UI
 		}
 	}
 
+	static auto toString(VkFormat format) -> const char*
+	{
+		switch (format)
+		{
+		default:
+			return "Undefined";
+		case VK_FORMAT_UNDEFINED:
+			return "Undefined";
+		case VK_FORMAT_R8G8B8A8_UNORM:
+			return "RGBA8_UNORM";
+		case VK_FORMAT_B8G8R8A8_UNORM:
+			return "BGRA8_UNORM";
+		case VK_FORMAT_R8G8B8A8_SRGB:
+			return "RGBA8_SRGB";
+		case VK_FORMAT_B8G8R8A8_SRGB:
+			return "BGRA8_SRGB";
+		case VK_FORMAT_R16G16B16A16_SFLOAT:
+			return "RGBA16_SFLOAT";
+		case VK_FORMAT_R32G32B32A32_SFLOAT:
+			return "RGBA32_SFLOAT";
+		case VK_FORMAT_D32_SFLOAT:
+			return "D32_SFLOAT";
+		case VK_FORMAT_D32_SFLOAT_S8_UINT:
+			return "D32_SFLOAT_S8_UINT";
+
+		}
+	}
+
 	void RendererLayer::onGuiRender()
 	{
 		if (!ImGui::Begin("Renderer"))
@@ -85,17 +113,16 @@ namespace Aegix::UI
 		ImGui::SeparatorText("Frame Graph Resources");
 
 		// Frame graph resources
-		const ImGuiTableFlags flags = ImGuiTableFlags_ScrollY | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter |
-			ImGuiTableFlags_BordersV | ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable |
-			ImGuiTableFlags_Hideable | ImGuiTableFlags_SizingFixedFit;
+		const ImGuiTableFlags flags = ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_BordersOuterH;
 		const ImVec2 outer_size = ImVec2(0.0f, ImGui::GetTextLineHeightWithSpacing() * 10);
-		if (ImGui::BeginTable("Resources", 4, flags, outer_size))
+		if (ImGui::BeginTable("Resources", 5, flags, outer_size))
 		{
 			ImGui::TableSetupScrollFreeze(0, 1); // Make top row always visible
-			ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_None);
+			ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_WidthFixed);
 			ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthStretch);
-			ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_None);
-			ImGui::TableSetupColumn("Producer", ImGuiTableColumnFlags_None);
+			ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed);
+			ImGui::TableSetupColumn("Format", ImGuiTableColumnFlags_WidthFixed);
+			ImGui::TableSetupColumn("Producer", ImGuiTableColumnFlags_WidthFixed);
 			ImGui::TableHeadersRow();
 
 			for (const auto& res : resourcePool.resources())
@@ -108,6 +135,12 @@ namespace Aegix::UI
 				ImGui::TableSetColumnIndex(2);
 				ImGui::Text("%s", toString(res.type));
 				ImGui::TableSetColumnIndex(3);
+				if (res.type == Graphics::FrameGraphResourceType::Texture)
+				{
+					auto& info = std::get<Graphics::FrameGraphResourceTextureInfo>(res.info);
+					ImGui::Text("%s", toString(info.format));
+				}
+				ImGui::TableSetColumnIndex(4);
 				ImGui::Text("%s", resourcePool.node(res.producer).name.c_str());
 			}
 
