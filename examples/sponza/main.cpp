b83dd0281ce6e0179aa51fa157cee8129a970037
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-03 16:16:54 +0100

    Add sponza example scene

---
+++ examples/sponza/main.cpp
@@ -0,0 +1,37 @@
+#include "core/engine.h"
+#include "scene/description.h"
+
+class Sponza : public Aegix::Scene::Description
+{
+public:
+	void initialize(Aegix::Scene::Scene& scene) override
+	{
+		using namespace Aegix;
+
+		// CAMERA
+		scene.mainCamera().component<Transform>() = Transform{
+			.location = { -9.75f, 1.2f, 5.25f},
+			.rotation = { glm::radians(-12.0f), 0.0f, glm::radians(263.0f) }
+		};
+
+		// MODELS
+		scene.load(ASSETS_DIR "Sponza/Sponza.gltf");
+
+		// LIGHTS
+		auto light = scene.createEntity("Point Light 1", { -5.0f, 0.3f, 7.0f });
+		light.addComponent<PointLight>(glm::vec3{ 1.0f, 1.0f, 1.0f }, 50.0f);
+
+		auto light2 = scene.createEntity("Point Light 2", { 4.0f, 0.3f, 7.0f });
+		light2.addComponent<PointLight>(glm::vec3{ 1.0f, 1.0f, 1.0f }, 50.0f);
+
+		auto light3 = scene.createEntity("Point Light 3", { -0.5f, 0.3f, 7.0f });
+		light3.addComponent<PointLight>(glm::vec3{ 1.0f, 1.0f, 1.0f }, 50.0f);
+	}
+};
+
+auto main() -> int
+{
+	Aegix::Engine engine;
+	engine.loadScene<Sponza>();
+	engine.run();
+}
