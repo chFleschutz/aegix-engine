8f20e0acaea03a564eae2d6c15dfbfeb358f5e0f
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-03 11:26:56 +0100

    Fix simple scene
    Remove old asset manager

--- examples/simple-scene/simple_scene.h
+++ examples/simple-scene/simple_scene.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "core/asset_manager.h"
 #include "graphics/systems/default_render_system.h"
 #include "graphics/systems/point_light_system.h"
 #include "scene/components.h"
@@ -16,7 +15,7 @@ class Rotator : public Aegix::Scripting::ScriptBase
 protected:
 	void update(float deltaSeconds) override
 	{
-		component<Aegix::Transform>().rotation += glm::vec3{ 0.0f, 0.0f, 1.0f } * deltaSeconds;
+		component<Aegix::Transform>().rotation += glm::vec3{ 0.0f, 0.0f, 1.0f } *deltaSeconds;
 	}
 };
 
@@ -30,23 +29,24 @@ public:
 	{
 		using namespace Aegix;
 
-		auto& assetManager = Aegix::AssetManager::instance();
+		auto& renderer = Engine::instance().renderer();
+		renderer.addRenderSystem<Aegix::Graphics::PointLightSystem>();
 
 		// MODELS
-		auto teapotMesh = assetManager.createModel("models/teapot.obj");
-		auto planeMesh = assetManager.createModel("models/plane.obj");
+		auto teapotMesh = Graphics::StaticMesh::create("models/teapot.obj");
+		auto planeMesh = Graphics::StaticMesh::create("models/plane.obj");
 
 		// MATERIALS
-		auto textureBlack = assetManager.createTexture(glm::vec4{ 0.0f }, 1, 1, VK_FORMAT_R8G8B8A8_UNORM);
-		auto textureWhite = assetManager.createTexture(glm::vec4{ 1.0f }, 1, 1, VK_FORMAT_R8G8B8A8_UNORM);
-		auto defaultNormal = assetManager.createTexture(glm::vec4{ 0.5f, 0.5f, 1.0f, 1.0f }, 1, 1, VK_FORMAT_R8G8B8A8_UNORM);
+		auto textureBlack = Graphics::Texture::create({ 1, 1 }, glm::vec4{ 0.0f }, VK_FORMAT_R8G8B8A8_UNORM);
+		auto textureWhite = Graphics::Texture::create({ 1, 1 }, glm::vec4{ 1.0f }, VK_FORMAT_R8G8B8A8_UNORM);
+		auto defaultNormal = Graphics::Texture::create({ 1, 1 }, glm::vec4{ 0.5f, 0.5f, 1.0f, 1.0f }, VK_FORMAT_R8G8B8A8_UNORM);
 
-		auto paintingTexture = assetManager.createTexture("textures/painting.png");
-		auto paintingMat = assetManager.createMaterialInstance<Aegix::Graphics::DefaultMaterial>(
+		auto paintingTexture = Graphics::Texture::create("textures/painting.png", VK_FORMAT_R8G8B8A8_SRGB);
+		auto paintingMat = renderer.createMaterialInstance<Aegix::Graphics::DefaultMaterial>(
 			paintingTexture, defaultNormal, textureWhite, textureBlack, textureBlack);
 
-		auto metalTexture = assetManager.createTexture("textures/brushed-metal.png");
-		auto metalMat = assetManager.createMaterialInstance<Aegix::Graphics::DefaultMaterial>(
+		auto metalTexture = Graphics::Texture::create("textures/brushed-metal.png", VK_FORMAT_R8G8B8A8_SRGB);
+		auto metalMat = renderer.createMaterialInstance<Aegix::Graphics::DefaultMaterial>(
 			metalTexture, defaultNormal, textureWhite, textureBlack, textureBlack);
 
 		// ENTITIES
@@ -62,10 +62,9 @@ public:
 		teapot.component<Aegix::Transform>().scale = glm::vec3{ 2.0f, 2.0f, 2.0f };
 
 		// LIGHTS
-		assetManager.addRenderSystem<Aegix::Graphics::PointLightSystem>();
-
 		constexpr int lightCount = 32;
 		constexpr float lightRadius = 10.0f;
+
 		// Ring of lights
 		for (int i = 0; i < lightCount; i++)
 		{
