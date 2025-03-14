d8f904a9967116627d1e01bf42ef10e0090b6df9
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-14 15:08:24 +0100

    Default generate brdf lut

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -106,5 +106,6 @@ namespace Aegix::Scene
 		env.prefiltered = std::make_shared<Graphics::Texture>(Engine::instance().device());
 		env.prefiltered->createCube(1, 1, VK_FORMAT_R16G16B16A16_SFLOAT, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, 1);
 		env.prefiltered->image().fillSFLOAT(glm::vec4{ 0.0f, 0.0f, 0.0f, 1.0f });
+		env.brdfLUT = Graphics::Texture::createBRDFLUT();
 	}
 }
