feed2c0fa031e5123185e770e1750ee6a9584d8b
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2025-03-16 17:39:54 +0100

    Update submodule glm

--- src/aegix/math/math.h
+++ src/aegix/math/math.h
@@ -2,6 +2,7 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/quaternion.hpp>
