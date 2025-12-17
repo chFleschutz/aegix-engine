de6ddc665b78d652b2984091c5c55d239e76ce17
Christoph Fleschutz <ch.fleschutz@gmail.com>
2025-12-17 19:21:55 +0100

    Fix frustum plane calc

--- src/aegix/graphics/frustum.h
+++ src/aegix/graphics/frustum.h
@@ -1,34 +1,47 @@
 #pragma once
 
 #include "scene/components.h"
-
-#include <glm/glm.hpp>
+#include "math/math.h"
 
 namespace Aegix::Graphics
 {
 	struct Frustum
 	{
-		// Plane equation: Ax + By + Cz + D = 0  -> represented as (A, B, C, D)
+		enum Side
+		{
+			Left = 0,
+			Right = 1,
+			Bottom = 2,
+			Top = 3,
+			Near = 4,
+			Far = 5
+		};
+
+		// Plane equation: Ax + By + Cz + D = 0  -> represented as { A, B, C, D }
 		using Plane = glm::vec4;
 
 		std::array<Plane, 6> planes;
 
 		static auto extractFrom(const glm::mat4& viewProjection) -> Frustum
 		{
-			Frustum frustum{
-				.planes = {
-					Frustum::Plane{ viewProjection[3] + viewProjection[0] }, // Left plane
-					Frustum::Plane{ viewProjection[3] - viewProjection[0] }, // Right plane
-					Frustum::Plane{ viewProjection[3] + viewProjection[1] }, // Bottom plane
-					Frustum::Plane{ viewProjection[3] - viewProjection[1] }, // Top plane
-					Frustum::Plane{ viewProjection[2]					  }, // Near plane (Vulkan specific: Z is [0, 1])
-					Frustum::Plane{ viewProjection[3] - viewProjection[2] }  // Far plane
-				}
-			};
+			auto row0 = glm::row(viewProjection, 0);
+			auto row1 = glm::row(viewProjection, 1);
+			auto row2 = glm::row(viewProjection, 2);
+			auto row3 = glm::row(viewProjection, 3);
+
+			Frustum frustum{};
+			frustum.planes[Left]   = row3 + row0;
+			frustum.planes[Right]  = row3 - row0;
+			frustum.planes[Bottom] = row3 + row1;
+			frustum.planes[Top]	   = row3 - row1;
+			frustum.planes[Near]   = row2;	  // Note: Vulkan uses z depth range [0, 1]
+			frustum.planes[Far]	   = row3 - row2;
 
 			// Normalize planes
 			for (auto& plane : frustum.planes)
 			{
+				float len = glm::length(glm::vec3(plane));
+				AGX_ASSERT_X(len > 0.0f, "Frustum Extraction: Plane has zero length (Div by 0 hazard)");
 				plane /= glm::length(glm::vec3(plane));
 			}
 
--- src/aegix/math/math.h
+++ src/aegix/math/math.h
@@ -5,6 +5,7 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_access.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/euler_angles.hpp>
