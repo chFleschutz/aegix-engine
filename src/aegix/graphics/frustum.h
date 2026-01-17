ea6018a78e5fccaf0206980f387cb598be67b2ba
Christoph Fleschutz <ch.fleschutz@gmail.com>
2026-01-17 10:48:26 +0100

    Refactor frustum

--- src/aegix/graphics/frustum.h
+++ src/aegix/graphics/frustum.h
@@ -7,16 +7,6 @@ namespace Aegix::Graphics
 {
 	struct Frustum
 	{
-		enum Side
-		{
-			Left = 0,
-			Right = 1,
-			Bottom = 2,
-			Top = 3,
-			Near = 4,
-			Far = 5
-		};
-
 		// Plane equation: Ax + By + Cz + D = 0  -> represented as { A, B, C, D }
 		using Plane = glm::vec4;
 
@@ -29,20 +19,21 @@ namespace Aegix::Graphics
 			auto row2 = glm::row(viewProjection, 2);
 			auto row3 = glm::row(viewProjection, 3);
 
-			Frustum frustum{};
-			frustum.planes[Left]   = row3 + row0;
-			frustum.planes[Right]  = row3 - row0;
-			frustum.planes[Bottom] = row3 + row1;
-			frustum.planes[Top]	   = row3 - row1;
-			frustum.planes[Near]   = row2;	  // Note: Vulkan uses z depth range [0, 1]
-			frustum.planes[Far]	   = row3 - row2;
+			Frustum frustum{
+				row3 + row0,
+				row3 - row0,
+				row3 + row1,
+				row3 - row1,
+				row2,	  // Note: Vulkan uses z depth range [0, 1]
+				row3 - row2,
+			};
 
 			// Normalize planes
 			for (auto& plane : frustum.planes)
 			{
 				float len = glm::length(glm::vec3(plane));
 				AGX_ASSERT_X(len > 0.0f, "Frustum Extraction: Plane has zero length (Div by 0 hazard)");
-				plane /= glm::length(glm::vec3(plane));
+				plane /= len;
 			}
 
 			return frustum;
